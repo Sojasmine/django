d75115827cb5660623999d591311efba3d5d3584
Sojasmine <sojasmine.gjerstad@gmail.com>
2021-07-26 20:06:24 +0000

    Project failed

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-bx4u(uj772xs06gkdd&7=s1vek(k9($h2vdh*mu8)a)4z7*2=8')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
